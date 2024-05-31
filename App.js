545486c0be50a7910192c715fec45004c44ca887
Jan Phandoh <janphandoh@gmail.com>
2024-05-31 17:49:11 +0000

    Text made bold

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>
-        My name is <Text style={styles.text2}>Kwabena.</Text>
+        My name is <Text style={styles.text2}>Kwabena Phandoh</Text>
       </Text>
       <StatusBar style="auto" />
     </View>
